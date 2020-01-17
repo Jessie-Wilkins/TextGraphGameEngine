c82d3cf22d078f569cccfe917af878af38268b0b
Jessie Wilkins <jessiekwilkins.yahoo.com@gmail.com>
2020-01-16 20:34:52 -0600

    Refactored various functions by reducing redundancy and breaking functions apart.

--- TGGE_Graphics.cpp
+++ TGGE_Graphics.cpp
@@ -58,68 +58,35 @@ int TGGE_Graphics::findLocationOfLastLineBreak(int& j, string str_graph, int loc
             if (j==vertical_location) {
                 return location_of_line_break;
             }
-            else if (j>vertical_location) {
-                return location_of_line_break;
+            else if(j>vertical_location) {
+                break;
             }
+            
         }
+        
     }
+    return location_of_line_break;
 }
 
 string TGGE_Graphics::insertCharactersIntoString(int j, string str_graph, int location_of_line_break, int vertical_location, int horizontal_location, char letter) {
     if(vertical_location == 0) {
-        if(location_of_line_break == 0) {
-            string temp_string = str_graph;
-
-            str_graph="";
-
-            for(int i = 0; i<horizontal_location; i++) {
-                str_graph += " ";
-            }
-            str_graph = str_graph+temp_string;
-        }
-        else {
-            string tempString1 = str_graph.substr(0, location_of_line_break);
-            string tempString2 = str_graph.substr(location_of_line_break, str_graph.size());
-            while(tempString1.size() < horizontal_location) {
-                tempString1+=" ";
-            }
-            str_graph = tempString1+tempString2;
-        }
-
-        str_graph.insert(horizontal_location,string(1,letter));
-        
+        str_graph = addCharacterToFirstLine(str_graph, location_of_line_break, horizontal_location, letter);
     }
     
     else if(j!=vertical_location) {
-        str_graph += "\n";
-        for(int i = 0; i <horizontal_location; i++) {
-            str_graph+=" ";
-        }
-        str_graph+=string(1,letter);
+        str_graph = addCharacterToNewLine(str_graph, horizontal_location, letter);
     }
 
     else {
-        int next_line_break = 0;
-        int next_vertical_location = vertical_location+1;
-        int next_location_of_line_break = 0;
-        next_location_of_line_break = findLocationOfLastLineBreak(next_line_break, str_graph, next_location_of_line_break, next_vertical_location);
-        if(ifDifferentLocation(location_of_line_break, horizontal_location, next_location_of_line_break)) {
-            str_graph = changeStringStructure(str_graph, location_of_line_break, next_location_of_line_break, horizontal_location);
-        }
-        str_graph.insert(location_of_line_break+1+horizontal_location,string(1,letter));
+        str_graph = addCharacterBetweenFirstAndLastLine(str_graph, vertical_location, letter, location_of_line_break, horizontal_location);
     }
     return str_graph;
 }
 
 string TGGE_Graphics::adjustCurrentStringGraphics(string str_graph, int location_of_line_break, int vertical_location, int horizontal_location, char letter) {
-    //if(vertical_location == 2) {
-            int j = 0;
-            location_of_line_break = findLocationOfLastLineBreak(j, str_graph, location_of_line_break, vertical_location);
-            str_graph = insertCharactersIntoString(j, str_graph, location_of_line_break, vertical_location, horizontal_location, letter);
-    //}
-    /*else {
-        str_graph.insertCharacters(horizontal_location+vertical_location, string(1,letter));
-    }*/
+    int j = 0;
+    location_of_line_break = findLocationOfLastLineBreak(j, str_graph, location_of_line_break, vertical_location);
+    str_graph = insertCharactersIntoString(j, str_graph, location_of_line_break, vertical_location, horizontal_location, letter);
     return str_graph;
 }
 
@@ -148,5 +115,41 @@ string TGGE_Graphics::addNewLineToBeginning(string str_print) {
     str_print = "\n";
     str_print = str_print+temp_string;
     return str_print;
+}
+
+string TGGE_Graphics::addCharacterToFirstLine(string str_graph, int location_of_line_break, int horizontal_location, char letter) {
+    string tempString1 = str_graph.substr(0, location_of_line_break);
+    string tempString2 = str_graph.substr(location_of_line_break, str_graph.size());
+    while(tempString1.size() < horizontal_location) {
+        tempString1+=" ";
+    }
+    str_graph = tempString1+tempString2;
+
+    str_graph.insert(horizontal_location,string(1,letter));
+
+    return str_graph;
+}
+
+string TGGE_Graphics::addCharacterToNewLine(string str_graph, int horizontal_location, char letter) {
+    str_graph += "\n";
+    for(int i = 0; i <horizontal_location; i++) {
+        str_graph+=" ";
+    }
+    str_graph+=string(1,letter);
+
+    return str_graph;
+}
+
+string TGGE_Graphics::addCharacterBetweenFirstAndLastLine(string str_graph, int vertical_location, char letter, int location_of_line_break, int horizontal_location) {
+    int next_line_break = 0;
+    int next_vertical_location = vertical_location+1;
+    int next_location_of_line_break = 0;
+    next_location_of_line_break = findLocationOfLastLineBreak(next_line_break, str_graph, next_location_of_line_break, next_vertical_location);
+    if(ifDifferentLocation(location_of_line_break, horizontal_location, next_location_of_line_break)) {
+        str_graph = changeStringStructure(str_graph, location_of_line_break, next_location_of_line_break, horizontal_location);
+    }
+    str_graph.insert(location_of_line_break+1+horizontal_location,string(1,letter));
+
+    return str_graph;
 }
 
